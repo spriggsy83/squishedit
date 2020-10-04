2b1a5efa00c2630a3e8f8697a0f09b3b3c3b7eb4
Andrew Spriggs <spriggsy83@yahoo.com.au>
2020-10-04 18:14:40 +1100

    Package updates

--- backend/schemas/garments.js
+++ backend/schemas/garments.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 exports.create = Joi.object().keys({
   name: Joi.string().required(),
@@ -10,88 +10,39 @@ exports.create = Joi.object().keys({
   sizeLabel: Joi.string().required(),
   lengths: Joi.string().allow(null),
   notes: Joi.string().allow(null),
-  aMin: Joi.number()
-    .positive()
-    .allow(null),
-  aMax: Joi.number()
-    .greater(Joi.ref('aMin'))
-    .allow(null),
-  bMin: Joi.number()
-    .positive()
-    .allow(null),
-  bMax: Joi.number()
-    .greater(Joi.ref('bMin'))
-    .allow(null),
-  b1Min: Joi.number()
-    .positive()
-    .allow(null),
-  b1Max: Joi.number()
-    .greater(Joi.ref('b1Min'))
-    .allow(null),
-  cMin: Joi.number()
-    .positive()
-    .allow(null),
-  cMax: Joi.number()
-    .greater(Joi.ref('cMin'))
-    .allow(null),
-  c1Min: Joi.number()
-    .positive()
-    .allow(null),
-  c1Max: Joi.number()
-    .greater(Joi.ref('c1Min'))
-    .allow(null),
-  dMin: Joi.number()
-    .positive()
-    .allow(null),
-  dMax: Joi.number()
-    .greater(Joi.ref('dMin'))
-    .allow(null),
-  eMin: Joi.number()
-    .positive()
-    .allow(null),
-  eMax: Joi.number()
-    .greater(Joi.ref('eMin'))
-    .allow(null),
-  e1Min: Joi.number()
-    .positive()
-    .allow(null),
-  e1Max: Joi.number()
-    .greater(Joi.ref('e1Min'))
-    .allow(null),
-  fMin: Joi.number()
-    .positive()
-    .allow(null),
-  fMax: Joi.number()
-    .greater(Joi.ref('fMin'))
-    .allow(null),
-  gMin: Joi.number()
-    .positive()
-    .allow(null),
-  gMax: Joi.number()
-    .greater(Joi.ref('gMin'))
-    .allow(null),
-  yMin: Joi.number()
-    .positive()
-    .allow(null),
-  yMax: Joi.number()
-    .greater(Joi.ref('yMin'))
-    .allow(null),
+  aMin: Joi.number().positive().allow(null),
+  aMax: Joi.number().greater(Joi.ref('aMin')).allow(null),
+  bMin: Joi.number().positive().allow(null),
+  bMax: Joi.number().greater(Joi.ref('bMin')).allow(null),
+  b1Min: Joi.number().positive().allow(null),
+  b1Max: Joi.number().greater(Joi.ref('b1Min')).allow(null),
+  cMin: Joi.number().positive().allow(null),
+  cMax: Joi.number().greater(Joi.ref('cMin')).allow(null),
+  c1Min: Joi.number().positive().allow(null),
+  c1Max: Joi.number().greater(Joi.ref('c1Min')).allow(null),
+  dMin: Joi.number().positive().allow(null),
+  dMax: Joi.number().greater(Joi.ref('dMin')).allow(null),
+  eMin: Joi.number().positive().allow(null),
+  eMax: Joi.number().greater(Joi.ref('eMin')).allow(null),
+  e1Min: Joi.number().positive().allow(null),
+  e1Max: Joi.number().greater(Joi.ref('e1Min')).allow(null),
+  fMin: Joi.number().positive().allow(null),
+  fMax: Joi.number().greater(Joi.ref('fMin')).allow(null),
+  gMin: Joi.number().positive().allow(null),
+  gMax: Joi.number().greater(Joi.ref('gMin')).allow(null),
+  yMin: Joi.number().positive().allow(null),
+  yMax: Joi.number().greater(Joi.ref('yMin')).allow(null),
 });
 
 exports.fitQuery = Joi.object().keys({
-  margin: Joi.number()
-    .min(0)
-    .allow(null)
-    .default(0),
+  margin: Joi.number().min(0).allow(null).default(0),
   measurements: Joi.array()
     .items(
       Joi.object({
         point: Joi.string()
           .required()
           .valid('a', 'b', 'b1', 'c', 'c1', 'd', 'e', 'e1', 'f', 'g', 'y'),
-        value: Joi.number()
-          .positive()
-          .required(),
+        value: Joi.number().positive().required(),
       }),
     )
     .allow(null),
--- backend/schemas/queries.js
+++ backend/schemas/queries.js
@@ -1,18 +1,11 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 const pagingSchema = Joi.object().keys({
-  offset: Joi.number()
-    .integer()
-    .min(0)
-    .default(0),
+  offset: Joi.number().integer().min(0).default(0),
   limit: Joi.alternatives()
     .try(
-      Joi.string()
-        .valid('all')
-        .insensitive(),
-      Joi.number()
-        .integer()
-        .positive(),
+      Joi.string().valid('all').insensitive(),
+      Joi.number().integer().positive(),
     )
     .default(10),
 });
@@ -20,20 +13,14 @@ const pagingSchema = Joi.object().keys({
 const filterSchema = Joi.array().items(
   Joi.object().keys({
     field: Joi.string().required(),
-    op: Joi.string()
-      .valid('=', 'ilike')
-      .insensitive()
-      .default('='),
+    op: Joi.string().valid('=', 'ilike').insensitive().default('='),
     value: Joi.alternatives().try(Joi.string(), Joi.number()),
   }),
 );
 
 const orderSchema = Joi.object().keys({
   field: Joi.string().allow(null),
-  dir: Joi.string()
-    .valid('asc', 'desc')
-    .insensitive()
-    .default('asc'),
+  dir: Joi.string().valid('asc', 'desc').insensitive().default('asc'),
 });
 
 module.exports = Joi.object({
